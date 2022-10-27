54e56cea0069563056007e66e402bd4127019ccd
Batyr Jumageldiyew <jumageldiyewb@list.ru>
2022-10-27 17:09:36 +0500

    fix

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from itertools import combinations
-
-
 def max_profit(data: list):
-    #
-    # Write your code here.
-    #
     min_value = 0
     max_value = 0
     current_value = 0
@@ -24,16 +18,6 @@ def max_profit(data: list):
 
 def main():
     data = [10, 9, 8, 8, 7, 4, 5, 5]
-
-    # result = {}
-    # for i, item in enumerate(data):
-    #     last_index = data[i + 1:]
-    #     if not last_index:
-    #         break
-    #     max_profit = max(data[i + 1:])
-    #     result[item] = {
-    #         'max_profit':
-    #     }
 
     print(max_profit(data))
 
